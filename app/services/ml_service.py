616585aca6d424418ba95924d13df9d6147f6a06
MarioCanudas <mario.cz24@outlook.com>
2026-03-03 21:01:37 -0600

    feat: Implement MLService class with model and preprocessor loading functionality

--- app/services/ml_service.py
+++ app/services/ml_service.py
@@ -1,8 +1,16 @@
 from typing import Optional
 
+import joblib
+import xgboost as xgb
+from core import ML_MODEL_PATH, PREPROCESSOR_PATH
+from sklearn.compose import ColumnTransformer
+
 
 class MLService:
     "Singleton class for ML model and scaler"
+
+    preprocessor: ColumnTransformer
+    model: xgb.XGBClassifier
 
     _instance: Optional["MLService"] = None
     _initialized: bool = False
@@ -19,3 +27,8 @@ class MLService:
 
     def _load_artifacts(self) -> None:
         "Load the preprocessor and model artifacts to process data"
+        # Load sklearn preprocessor
+        self.preprocessor = joblib.load(PREPROCESSOR_PATH)
+
+        self.model = xgb.XGBClassifier()
+        self.model.load_model(ML_MODEL_PATH)
