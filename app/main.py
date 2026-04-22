34fdf34e6b025f01cad7921f7c8dd3d0fc8d9b85
MarioCanudas <mario.cz24@outlook.com>
2026-04-22 11:35:41 -0600

    chore: Simplify main fastapi file

--- app/main.py
+++ app/main.py
@@ -1,9 +1,3 @@
 from fastapi import FastAPI
-from schemas import Message
 
 app = FastAPI()
-
-
-@app.get(path="/", response_model=Message)
-async def root() -> Message:
-    return Message(title="test", body="Hello world")
